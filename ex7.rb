524c21144f5c94a20587a0559bc226a26d6dbcad
Jack Yuan <Jack@jackyuan.com>
2014-05-04 18:01:09 -0400

    Exercise 7 completed

---
+++ ex7.rb
@@ -0,0 +1,15 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "."*10
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+
+print end1+end2+end3+end4+end5+end6
+
+puts
